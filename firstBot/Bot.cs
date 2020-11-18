0b221170fb68b5de4540841da3b0ae42c1efd42e
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-11-18 19:14:04 +0545

    json init

---
+++ firstBot/Bot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+
+namespace firstBot
+{
+    class Bot
+    {
+    }
+}
--- firstBot/Program.cs
+++ firstBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace firstBot
 {
@@ -6,7 +7,30 @@ namespace firstBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (ConfigInitialization())
+            {
+
+            }
+            else
+                Console.WriteLine("Please fill in the valid details in generated json file");
+        }
+
+        static bool ConfigInitialization()
+        {
+            try
+            {
+                File.Open("config.json", FileMode.Open);
+            }
+            catch (FileNotFoundException _)
+            {
+                string data = "{\n  \"token\": \"\",\n  \"prefix\": \"\"\n}";
+                using (var json = new StreamWriter("config.json"))
+                {
+                    json.Write(data);
+                }
+                return false;
+            }
+            return true;
         }
     }
 }
