d95680c0c1849eb9da97949b237a2114ac6c1596
Hungerarray <59282692+Hungerarray@users.noreply.github.com>
2020-11-18 21:48:08 +0545

    Basic commands

--- firstBot/Commands/myCommands.cs
+++ firstBot/Commands/myCommands.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Interactivity;
 
 namespace firstBot.Commands
 {
     class myCommands
     {
+        [Command("ping")]
+        [Description("returns the ping")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.RespondAsync(ctx.Client.Ping.ToString());
+        }
+
+        [Command("hi")]
+        [Description("greet user")]
+        [Hidden]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.RespondAsync($":wave: Hello {ctx.User.Mention}!! (●'◡'●)");
+        }
+
+        [Command("random")]
+        [Description("Return a random number between min and max")]
+        public async Task Random(CommandContext ctx, 
+            [Description("minimum limit")]int min, 
+            [Description("maximum limit")] int max)
+        {
+            var rand = new Random();
+            if (min > max)
+                await ctx.RespondAsync("incorrect limit");
+            else
+                await ctx.RespondAsync(rand.Next(min, max).ToString());
+        }
     }
 }
